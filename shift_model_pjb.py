e0181f29d2743f35a26c16bf73394802174b9d1c
Peter Bekins <65368241+peterbekins@users.noreply.github.com>
2020-11-29 18:55:48 -0500

    Update shift_model_pjb.py
    added vacation constraint

--- shift_model_pjb.py
+++ shift_model_pjb.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+np.random.seed(441)
+
+
 # timer to measure computational time
 time_start = time.perf_counter()
 
@@ -11,27 +14,37 @@ time_start = time.perf_counter()
 residents = 4
 weeks = 4
 shifts = weeks * 21
-
+shift_requests = []
 
 # initialize shift requests with random variable for now
 # this algorithm has each resident rate each shift 1-3
 # I choose without replacement to ensure that the requests are similar
 # each week, each resident will have 7 shifts rated 3, etc. 
 week_ratings = [3,3,3,3,3,3,3,2,2,2,2,2,2,2,1,1,1,1,1,1,1]
-ratings = np.repeat(week_ratings, weeks)
-shift_requests = []
+
 for r in range(residents):
     week_requests = []
     for w in range(weeks):
         week_requests.append(np.random.choice(week_ratings, 21, replace=False))
     shift_requests.append(np.hstack(week_requests))
-    
+
+# vacation requests, 1 = available to work 0 = on vacation
+v = []
+v.append(np.tile(1, shifts))
+v.append(np.tile(1, shifts))
+v.append(np.tile(1, shifts))
+v.append([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+          0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+          1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+          1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+
 # Set min/max for resident per shift
 min_per_shift = 1
-max_per_shift = 2
+max_per_shift = 3
 
 # Set max for resident total 
 max_per_week = weeks * 10
+min_per_week = weeks * 6
 
 # Create the model
 shift_model = cp_model.CpModel()
@@ -49,6 +62,13 @@ for r in range(residents):
     for s in range(shifts):
         y[(r,s)] = shift_model.NewBoolVar('y_r%s%i' % (r, s))
 
+# c. z_rwd is binary, indicator variable turns on if resident r is scheduled on week w day d
+    z = {}
+    for r in range(residents):
+        for w in range(0, shifts, 21):
+            for d in range(w, w + 21, 3):
+                z[(r,w,d)] = shift_model.NewBoolVar('z_r%iw%id%i' % (r,w,d))
+
 # 2. Contraints
     
 # a. Min and max residents per shift
@@ -61,7 +81,7 @@ for s in range(shifts):
 # b. Max shifts per week capped at 10 per resident but averaged over 4 weeks
 for r in range(residents):
     shift_model.Add(sum(x[(r,s)] for s in range(shifts)) <= max_per_week)
-
+    shift_model.Add(sum(x[(r,s)] for s in range(shifts)) >= min_per_week)
 
 # c. Resident must have 16 hours off after three consecutive shifts
 for r in range(residents):
@@ -81,21 +101,47 @@ for r in range(residents):
     edge = x[(r, shifts-1)] + x[(r, shifts-2)]+ x[(r, shifts-3)] + x[(r, shifts-4)] 
     shift_model.Add(edge < 4)
 
+# d. Resident must have one full day off per week 
+for r in range(residents):
+    for w in range(0, shifts, 21):
+        weekly_total = 0
+        for d in range(w, w+21, 3):
+            daily_total = x[(r, d)] + x[(r, d+1)] + x[(r,d+2)]
+                
+            #Turns z on if r works day d
+            shift_model.Add(daily_total >= 1 - (1 - z[(r,w,d)]) * 100)   
+            shift_model.Add(daily_total < 1 + z[(r,w,d)] * 100)
+            weekly_total += z[(r,w,d)]
+        #can work max 6 days a week
+        shift_model.Add(weekly_total <= 6)
+
+# e. Block vacation
+for r in range(residents):
+    for s in range(shifts):
+        shift_model.Add(x[(r,s)] <= v[r][s])
+        
 # 3. Objective function
 # Maximize the number of requested shifts assigned to each resident
 # Need to look into balancing across residents as well
-shift_model.Maximize(
+shift_model.Minimize(
         sum(shift_requests[r][s] * x[(r,s)] 
             for r in range(residents)
             for s in range(shifts)))
 
 # 4. Solver
 solver = cp_model.CpSolver()
-solver.Solve(shift_model)
+printer = cp_model.ObjectiveSolutionPrinter()
+status = solver.SolveWithSolutionCallback(shift_model, printer)
+
+if status == cp_model.FEASIBLE:
+    print("Feasible!")
+    print("Solution = ", solver.ObjectiveValue())
+else:
+    print("no solution")
 
 # 5. Grid plot of to show preference matrix
 plt.figure(figsize=(8,3))
-plt.imshow(shift_requests, cmap="Blues",aspect = 3)
+plt.imshow(shift_requests, cmap="Oranges",aspect = 3)
 plt.axvline(20.5, color='black')
 plt.axvline(41.5, color='black')
 plt.axvline(62.5, color='black')
@@ -129,7 +175,7 @@ for r in range(residents):
         if solver.Value(x[(r, s)]) == 1:
             value = value + shift_requests[r][s]
             tot_shifts = tot_shifts + 1
-            if shift_requests[r][s] > 1:
+            if shift_requests[r][s] < 3:
                 shift_result.append((128,128,128)) # dark gray for shift on
             else:
                 shift_result.append((204,51,0)) # reddish for on but didn't request
